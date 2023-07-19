149c480f0782aa123c69fe3fd8a57b45a67fae8c
Vrashabh Sontakke <86019029+Vrashabh-Sontakke@users.noreply.github.com>
2023-07-19 16:44:33 +0530

    Update main.tf

--- plan/cluster/main.tf
+++ plan/cluster/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "myterraformbucket12"
-    key    = "eks-terraform.tfstate"
+    key    = "eks-prod.tfstate"
     region = "us-east-1"
   }
   required_providers {
