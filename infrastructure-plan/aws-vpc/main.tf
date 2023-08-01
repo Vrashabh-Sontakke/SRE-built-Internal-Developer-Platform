891018d24735de77a9bd9dd5fd80c6b34597e541
Vrashabh Sontakke <86019029+Vrashabh-Sontakke@users.noreply.github.com>
2023-08-02 03:29:56 +0530

    fixed bugs in terraform config

--- infrastructure-plan/aws-vpc/main.tf
+++ infrastructure-plan/aws-vpc/main.tf
@@ -84,3 +84,19 @@ resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.pubsub2.id
   route_table_id = aws_route_table.rt.id
 }
+
+output "pubsub1_id" {
+  value = aws_subnet.pubsub1.id
+}
+
+output "pubsub2_id" {
+  value = aws_subnet.pubsub2.id
+}
+
+output "privsub1_id" {
+  value = aws_subnet.privsub1.id
+}
+
+output "privsub2_id" {
+  value = aws_subnet.privsub2.id
+}
--- infrastructure-plan/aws-vpc/variables.tf
+++ infrastructure-plan/aws-vpc/variables.tf
@@ -1,5 +1,4 @@
 variable "vpc_name" {
   type = string
-  default = "vpc"
 }
 
